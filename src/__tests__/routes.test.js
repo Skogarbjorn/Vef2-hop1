15af72642d52edceee2b02651f7680f4707f0bca
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-03-13 19:26:45 -0100

    alright now done

--- src/__tests__/routes.test.js
+++ src/__tests__/routes.test.js
@@ -1,6 +1,6 @@
 import request from "supertest";
 import app from "../app.js";
-import { expect, test } from "@jest/globals";
+import { beforeAll, expect, test } from "@jest/globals";
 
 let authToken = "";
 
