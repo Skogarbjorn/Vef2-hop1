d16c30345333697d62e1159811126572570b1e54
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-03-13 19:34:18 -0100

    fixed index.json and not requiring auth on PATCH /laera/[id]

--- src/api/index.js
+++ src/api/index.js
@@ -4,47 +4,47 @@ import { catchErrors } from "../lib/catchErrors.js";
 import { deleteUser, listUser, listUsers } from "./users.js";
 import { validationCheck } from "../validate/helpers.js";
 import {
-  agesValidator,
-  capacityValidator,
-  courseIdValidator,
-  dateValidator,
-  descriptionValidator,
-  durationValidator,
-  endDateValidator,
-  levelValidator,
-  moveDescriptionValidator,
-  moveIdValidator,
-  moveTitleValidator,
-  nameValidator,
-  pagingQuerystringValidator,
-  practiceCapacityNotFullValidator,
-  practiceIdValidator,
-  startDateValidator,
-  userNotSignedUpCourseValidator,
-  userNotSignedUpPracticeValidator,
+	agesValidator,
+	capacityValidator,
+	courseIdValidator,
+	dateValidator,
+	descriptionValidator,
+	durationValidator,
+	endDateValidator,
+	levelValidator,
+	moveDescriptionValidator,
+	moveIdValidator,
+	moveTitleValidator,
+	nameValidator,
+	pagingQuerystringValidator,
+	practiceCapacityNotFullValidator,
+	practiceIdValidator,
+	startDateValidator,
+	userNotSignedUpCourseValidator,
+	userNotSignedUpPracticeValidator,
 } from "../validate/validators.js";
 import {
-  addPractice,
-  deletePractice,
-  listPractice,
-  listPracticeSingular,
-  signToPractice,
-  updatePractice,
+	addPractice,
+	deletePractice,
+	listPractice,
+	listPracticeSingular,
+	signToPractice,
+	updatePractice,
 } from "./practice.js";
 import {
-  addCourse,
-  deleteCourse,
-  listCourse,
-  listCourses,
-  signToCourse,
-  updateCourse,
+	addCourse,
+	deleteCourse,
+	listCourse,
+	listCourses,
+	signToCourse,
+	updateCourse,
 } from "./courses.js";
 import {
-  addMove,
-  deleteMove,
-  listMove,
-  listMoves,
-  updateMove,
+	addMove,
+	deleteMove,
+	listMove,
+	listMoves,
+	updateMove,
 } from "./moves.js";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
@@ -54,188 +54,189 @@ import { withMulter } from "../lib/multer.js";
 export const router = express.Router();
 
 router.get("/", async (_req, res) => {
-  const path = dirname(fileURLToPath(import.meta.url));
-  const indexJson = await readFile(join(path, "./index.json"));
-  res.json(JSON.parse(indexJson));
+	const path = dirname(fileURLToPath(import.meta.url));
+	const indexJson = await readFile(join(path, "./index.json"));
+	res.json(JSON.parse(indexJson));
 });
 
 // PROFA
 router.get(
-  "/profa",
-  pagingQuerystringValidator,
-  validationCheck,
-  catchErrors(listPractice),
+	"/profa",
+	pagingQuerystringValidator,
+	validationCheck,
+	catchErrors(listPractice),
 );
 
 router.post(
-  "/profa",
-  requireAdmin,
-  dateValidator,
-  durationValidator,
-  agesValidator,
-  capacityValidator,
-  validationCheck,
-  catchErrors(addPractice),
+	"/profa",
+	requireAdmin,
+	dateValidator,
+	durationValidator,
+	agesValidator,
+	capacityValidator,
+	validationCheck,
+	catchErrors(addPractice),
 );
 
 // PROFA:ID
 router.get(
-  "/profa/:id",
-  requireAdmin,
-  pagingQuerystringValidator,
-  validationCheck,
-  catchErrors(listPracticeSingular),
+	"/profa/:id",
+	requireAdmin,
+	pagingQuerystringValidator,
+	validationCheck,
+	catchErrors(listPracticeSingular),
 );
 
 router.post(
-  "/profa/:id",
-  requireAuth,
-  practiceIdValidator,
-  practiceCapacityNotFullValidator,
-  userNotSignedUpPracticeValidator,
-  validationCheck,
-  catchErrors(signToPractice),
+	"/profa/:id",
+	requireAuth,
+	practiceIdValidator,
+	practiceCapacityNotFullValidator,
+	userNotSignedUpPracticeValidator,
+	validationCheck,
+	catchErrors(signToPractice),
 );
 
 router.delete(
-  "/profa/:id",
-  requireAdmin,
-  practiceIdValidator,
-  validationCheck,
-  catchErrors(deletePractice),
+	"/profa/:id",
+	requireAdmin,
+	practiceIdValidator,
+	validationCheck,
+	catchErrors(deletePractice),
 );
 
 router.patch(
-  "/profa/:id",
-  requireAdmin,
-  practiceIdValidator,
-  dateValidator,
-  durationValidator,
-  agesValidator,
-  capacityValidator,
-  validationCheck,
-  catchErrors(updatePractice),
+	"/profa/:id",
+	requireAdmin,
+	practiceIdValidator,
+	dateValidator,
+	durationValidator,
+	agesValidator,
+	capacityValidator,
+	validationCheck,
+	catchErrors(updatePractice),
 );
 
 // NAMSKEID
 router.get(
-  "/namskeid",
-  pagingQuerystringValidator,
-  validationCheck,
-  catchErrors(listCourses),
+	"/namskeid",
+	pagingQuerystringValidator,
+	validationCheck,
+	catchErrors(listCourses),
 );
 
 router.post(
-  "/namskeid",
-  requireAdmin,
-  nameValidator,
-  descriptionValidator,
-  levelValidator,
-  startDateValidator,
-  endDateValidator,
-  validationCheck,
-  catchErrors(addCourse),
+	"/namskeid",
+	requireAdmin,
+	nameValidator,
+	descriptionValidator,
+	levelValidator,
+	startDateValidator,
+	endDateValidator,
+	validationCheck,
+	catchErrors(addCourse),
 );
 
 // NAMSKEID:ID
 router.get(
-  "/namskeid/:id",
-  requireAdmin,
-  courseIdValidator,
-  pagingQuerystringValidator,
-  validationCheck,
-  catchErrors(listCourse),
+	"/namskeid/:id",
+	requireAdmin,
+	courseIdValidator,
+	pagingQuerystringValidator,
+	validationCheck,
+	catchErrors(listCourse),
 );
 
 router.post(
-  "/namskeid/:id",
-  requireAuth,
-  courseIdValidator,
-  userNotSignedUpCourseValidator,
-  validationCheck,
-  catchErrors(signToCourse),
+	"/namskeid/:id",
+	requireAuth,
+	courseIdValidator,
+	userNotSignedUpCourseValidator,
+	validationCheck,
+	catchErrors(signToCourse),
 );
 
 router.delete(
-  "/namskeid/:id",
-  requireAdmin,
-  courseIdValidator,
-  validationCheck,
-  catchErrors(deleteCourse),
+	"/namskeid/:id",
+	requireAdmin,
+	courseIdValidator,
+	validationCheck,
+	catchErrors(deleteCourse),
 );
 
 router.patch(
-  "/namskeid/:id",
-  requireAdmin,
-  courseIdValidator,
-  nameValidator,
-  descriptionValidator,
-  levelValidator,
-  startDateValidator,
-  endDateValidator,
-  validationCheck,
-  catchErrors(updateCourse),
+	"/namskeid/:id",
+	requireAdmin,
+	courseIdValidator,
+	nameValidator,
+	descriptionValidator,
+	levelValidator,
+	startDateValidator,
+	endDateValidator,
+	validationCheck,
+	catchErrors(updateCourse),
 );
 
 // LAERA
 router.get(
-  "/laera",
-  pagingQuerystringValidator,
-  validationCheck,
-  catchErrors(listMoves),
+	"/laera",
+	pagingQuerystringValidator,
+	validationCheck,
+	catchErrors(listMoves),
 );
 
 router.post(
-  "/laera",
-  requireAdmin,
-  withMulter,
-  moveTitleValidator,
-  moveDescriptionValidator,
-  validationCheck,
-  catchErrors(addMove),
+	"/laera",
+	requireAdmin,
+	withMulter,
+	moveTitleValidator,
+	moveDescriptionValidator,
+	validationCheck,
+	catchErrors(addMove),
 );
 
 // LAERA:ID
 router.get(
-  "/laera/:id",
-  moveIdValidator,
-  validationCheck,
-  catchErrors(listMove),
+	"/laera/:id",
+	moveIdValidator,
+	validationCheck,
+	catchErrors(listMove),
 );
 
 router.delete(
-  "/laera/:id",
-  requireAdmin,
-  moveIdValidator,
-  validationCheck,
-  catchErrors(deleteMove),
+	"/laera/:id",
+	requireAdmin,
+	moveIdValidator,
+	validationCheck,
+	catchErrors(deleteMove),
 );
 
 router.patch(
-  "/laera/:id",
-  withMulter,
-  moveIdValidator,
-  moveTitleValidator,
-  moveDescriptionValidator,
-  validationCheck,
-  catchErrors(updateMove),
+	"/laera/:id",
+	requireAdmin,
+	withMulter,
+	moveIdValidator,
+	moveTitleValidator,
+	moveDescriptionValidator,
+	validationCheck,
+	catchErrors(updateMove),
 );
 
 // USERS
 router.get(
-  "/users",
-  requireAdmin,
-  pagingQuerystringValidator,
-  validationCheck,
-  catchErrors(listUsers),
+	"/users",
+	requireAdmin,
+	pagingQuerystringValidator,
+	validationCheck,
+	catchErrors(listUsers),
 );
 
 // USERS:ID
 router.get("/users/:id", requireAdmin, validationCheck, catchErrors(listUser));
 
 router.delete(
-  "/users/:id",
-  requireAdmin,
-  validationCheck,
-  catchErrors(deleteUser),
+	"/users/:id",
+	requireAdmin,
+	validationCheck,
+	catchErrors(deleteUser),
 );
